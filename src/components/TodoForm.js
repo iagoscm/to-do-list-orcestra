1947f8ce321e205c9bb08232fb428f33f5d85f62
iagoscm <iagomatosousa@gmail.com>
2022-06-15 19:37:23 -0300

    finaliza cerne da lista de tarefas

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,7 +1,13 @@
-import React , {useState} from 'react'
+import React , {useState, useEffect, useRef} from 'react'
 
 function TodoForm(props) {
-const [input, setInput] = useState('');
+const [input, setInput] = useState(props.edit ? props.edit.value : '' );
+
+const inputRef = useRef(null)
+
+useEffect(() => {
+    inputRef.current.focus()
+})
 
 const handleChange = e => {
     setInput(e.target.value);
@@ -10,27 +16,41 @@ const handleChange = e => {
 const handleSubmit = e => {
     e.preventDefault();
 
-   /*  props.onSubmit({
+    props.onSubmit({
         id: Math.floor(Math.random() * 10000),
         text: input
-    }); */   
+    });
     
     setInput('');
 };
 
   return (
     <form className='todo-form' onSubmit={handleSubmit}>
-        <input
-        type='text'
-        placeholder='Add a todo'
-        value={input}
-        name='text'
-        className='todo-input'
-        onChange={handleChange}
-        />
-        <button className='todo-button'>Add todo</button>
+
+        {props.edit ? ( <><input
+                type='text'
+                placeholder='Edite sua tarefa'
+                value={input}
+                name='text'
+                className='todo-input'
+                onChange={handleChange}
+                ref={inputRef}
+            />
+        <button className='todo-button'>Salvar</button></>) : (    <><input
+                type='text'
+                placeholder='Adicione sua tarefa'
+                value={input}
+                name='text'
+                className='todo-input'
+                onChange={handleChange}
+                ref={inputRef}
+            />
+        <button className='todo-button'>Adicionar</button></>
+        )}
+
+        
     </form>
-);
-}
+    );
+};
 
 export default TodoForm;
